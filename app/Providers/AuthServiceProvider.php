6c9b86dc97b1cff56e41f33c9ad95b8c825b4b30
Muhammad Hannan <muhammad.hannan@binaryit.co>
2023-02-23 15:59:43 +0500

    checking new

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,8 @@
 namespace App\Providers;
 
 use Laravel\Passport\Passport;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +25,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-         Passport::routes();
+         Passport::ignoreRoutes();
     }
 }
